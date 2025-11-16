c6bd7c085f4e9fcafc37efbc23075edcdd4a3f90
pema4 <petelin.maksim@live.com>
2025-11-16 18:45:56 +0300

    sem-7

--- sem-7/README.md
+++ sem-7/README.md
@@ -6,14 +6,8 @@ DI контейнер, бины, environment.
 
 ## Примеры
 
-1. [TodoApp](./src/main/java/todo/server/todoApp.java) — настроенный Jetty-сервер
-2. [HttpRequest2](./src/main/java/HttpRequest2.java) — работа со встроенным `HttpClient`
-3. [HttpServerExample](./src/main/java/HttpServerExample.java) — работа со встроенным `HttpServer`
 
 ## Полезные ссылки/литература
 
-* глава 11 из Java Network Programming, Fourth Edition by Elliotte Rusty Harold
-* https://jetty.org/docs/jetty/12.1/programming-guide/server/http.html
-* https://jetty.org/docs/jetty/12.1/programming-guide/arch/threads.html
-* https://jetty.org/docs/jetty/12.1/programming-guide/arch/io.html
+* https://docs.spring.io/spring-framework/reference/overview.html
 
--- sem-7/src/test/java/todo/server/SpringTodoAppTest.java
+++ sem-7/src/test/java/todo/server/SpringTodoAppTest.java
@@ -1,8 +1,55 @@
 package todo.server;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.TestSocketUtils;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {SpringTodoApp.class})
 public class SpringTodoAppTest {
+    @Autowired
+    Server server;
+
+    @DynamicPropertySource
+    static void portProperty(DynamicPropertyRegistry propertySource) {
+        int availableTcpPort = TestSocketUtils.findAvailableTcpPort();
+        propertySource.add("test-port", () -> availableTcpPort);
+    }
+
+    @Test
+    void handlerTest() throws IOException, InterruptedException {
+        var port = ((ServerConnector) server.getConnectors()[0]).getPort();
+        try (var httpClient = HttpClient.newHttpClient()) {
+            var postRequest = HttpRequest.newBuilder(URI.create("http://localhost:" + port + "/todo/create"))
+                    .POST(HttpRequest.BodyPublishers.ofString("""
+                            {
+                              "text": "My todo item"
+                            }
+                            """))
+                    .build();
+            var postResponse = httpClient.send(postRequest, HttpResponse.BodyHandlers.ofString());
+            IO.println("POST Response: " + postResponse);
+            IO.println("POST Headers: " + postResponse.headers().map());
+            IO.println("POST Body: " + postResponse.body());
+
+            var getRequest = HttpRequest.newBuilder(URI.create("http://localhost:8080/todo/1")).build();
+            var getResponse = httpClient.send(getRequest, HttpResponse.BodyHandlers.ofString());
+            IO.println("GET Response: " + getResponse);
+            IO.println("GET Headers: " + getResponse.headers());
+            IO.println("GET Body: " + getResponse.body());
+        }
+    }
 }
